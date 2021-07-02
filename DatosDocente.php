a21b5a56377bbcdec8e0ff492fb07f3cc258fd6f
marianElizabeth <61371595+marianElizabeth@users.noreply.github.com>
2021-07-02 15:51:33 -0500

    Update DatosDocente.php

--- DatosDocente.php
+++ DatosDocente.php
@@ -124,12 +124,14 @@
 					<ul class="nav nav-tabs" style="margin-bottom: 15px;">				  	
 						<li class="active"><a href="Nuevo" data-toggle="tab">Nuevo</a></li>
 					</ul>
+
+
 					<div id="myTabContent" class="tab-content">
-						<div class="tab-pane fade active in" id="Nuevo">
-							<div class="container-fluid">
+                    <div class="container-fluid">
 								<div class="row">
 									<div class="col-xs-12 col-md-10 col-md-offset-1">
-									    <form action="">
+
+
 											<ul class="nav nav-tabs" style="margin-bottom: 15px;">
 												<li class="active"><a href="" data-toggle="tab" ><i class="zmdi zmdi-face zmdi-hc-fw"></i>Información Personal</a></li>
 										    </ul>
@@ -414,47 +416,75 @@
 												<ul class="nav nav-tabs" style="margin-bottom: 15px;">
 													<li class="active"><a href="" data-toggle="tab" ><i class="zmdi zmdi-city zmdi-hc-fw"></i>Escuela donde Labora</a></li>
 												</ul>
-													<div id="myTabContent" class="tab-content">
-														<div class="table-responsive">
-															<table class="table table-hover text-center">
-								                                <thead>
-									                            <tr>
-										                        <th class="text-center">Nombre de la escuela</th>
-										                        <th class="text-center">Puesto o Asignatura que atiende</th>
-										                        <th class="text-center">No. Horas</th>
-									                            </tr>
-								                                </thead>
-								                             <tbody>
-									                            <tr>
-										                        <td>
-																<select class="form-control" style="color: rgb(0, 0, 0); font-size: 100%;" name="cmbNombreEscuela">
-														            <option>--Seleccione--</option>
-														            <option>A</option>
-														            <option>B</option>
-														            <option>C</option>
-														            <option>D</option>
-														            <option>E</option>
-														        </select>
-																</td>
-										                        <td>
-																<select class="form-control" style="color: rgb(0, 0, 0); font-size: 100%;" name="cmbPuesto">
-														            <option>--Seleccione--</option>
-														            <option>A</option>
-														            <option>B</option>
-														            <option>C</option>
-														            <option>D</option>
-														            <option>E</option>
-														        </select>
-																</td>
-										                        <td>
-																    <input class="form-control"style="color: rgb(0, 0, 0); font-size: 100%;" type="text" name="txtNoHoras">
-																</td>
-									                            </tr>
-								                            </tbody>
-							                                </table>
-						                                </div>
-				                                    </div>  
-														  <div class="form-group label-floating">
+												<div class="position-relative">
+  <div class="position-absolute top-50 start-0 translate-middle-x">
+  <form action="DatosDocente.php" method="POST" class="row row-cols-lg-auto g-3 align-items-center">
+    <div class="form-group label-floating">
+			<label class="control-label" style="color: rgb(0, 0, 0); font-size: 120%;">Numero de Escuelas donde labora</label>
+			<input class="form-control" style="color: rgb(0, 0, 0); font-size: 100%;"  type="text" name="txtNumEscuelasLabora">
+		 </div>
+  <div class="col-12">
+  <input type="submit" value="Enviar" class="btn btn-primary" name = "btn1">
+  </div>
+  </form>
+  </div>
+  </div>
+<br>
+<br>
+<?php
+  if (isset($_POST['btn1']))
+  {
+    $numEscuelas=$_POST['txtNumEscuelasLabora'];
+    crearEcuelas($numEscuelas);
+  }
+  function crearEcuelas($nE){
+    for ($i = 0 ; $i<$nE ; $i++){
+      echo "<form action='DatosDocente.php' method='POST' class='row row-cols-lg-auto g-3 align-items-center'>";
+      echo "<div id='myTabContent'class='tab-content'>";
+			echo "<div class='table-responsive'>";
+			echo "<table class='table table-hover text-center'>";
+			echo "<thead>";
+	    echo "<tr>";
+			echo "<th class='text-center'>Nombre de la escuela</th>";
+			echo "<th class='text-center'>Puesto o Asignatura que atiende</th>";
+			echo "<th class='text-center'>No. Horas</th>";
+			echo "</tr";
+			echo "</thead>";
+			echo "<tbody>";
+			echo "<tr>";
+			echo "<td>";
+			echo "<select class='form-control' style='color: rgb(0, 0, 0); font-size: 100%;' name='cmbNombreEscuela'>";
+			echo "<option>--Seleccione--</option>";
+			echo "<option>A</option>";
+			echo "<option>B</option>";
+			echo "<option>C</option>";
+			echo "<option>D</option>";
+			echo "<option>E</option>";
+			echo "</select>";
+		  echo "</td>";
+			echo "<td>";
+			echo "<select class='form-control' style='color: rgb(0, 0, 0); font-size: 100%;' name='cmbPuesto'>";
+			echo "<option>--Seleccione--</option>";
+			echo "<option>A</option>";
+			echo "<option>B</option>";
+			echo "<option>C</option>";
+			echo "<option>D</option>";
+			echo "<option>E</option>";
+			echo "</select>";
+			echo "</td>";
+			echo "<td>";
+			echo "<input class='form-control'style='color: rgb(0, 0, 0); font-size: 100%;' type='text' name='txtNoHoras'>";
+			echo "</td>";
+			echo "</tr>";
+			echo "</tbody>";
+			echo "</table>";
+			echo "</div>";
+			echo "</div>";
+      echo "</form>";
+    }
+  }
+?>
+<div class="form-group label-floating">
 															<label class="control-label" style="color: rgb(0, 0, 0); font-size: 120%;">Número de Plaza</label>
 															<input class="form-control"style="color: rgb(0, 0, 0); font-size: 100%;" type="text" name="txtNoPlaza">
 														</div>	
@@ -470,6 +500,7 @@
 															<label class="control-label" style="color: rgb(0, 0, 0); font-size: 120%;">Número de Horas</label>
 															<input class="form-control"style="color: rgb(0, 0, 0); font-size: 100%;" type="text" name="txtNumHoras">
 														</div>
+
 										    <p class="text-center">
 										    	<button href="#!" class="btn btn-info btn-raised btn-sm" style="color: rgb(0, 0, 0); font-size: 100%;"><i class="zmdi zmdi-floppy"></i> GUARDAR CAMBIOS</button>
 										    </p>
