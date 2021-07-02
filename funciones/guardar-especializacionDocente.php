b8b91c50293cfca6a802a271f35a5da78e8a7bab
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-01 19:10:05 -0500

    Create guardar-especializacionDocente.php

---
+++ funciones/guardar-especializacionDocente.php
@@ -0,0 +1,23 @@
+<?php
+    include("funciones/conexion.php");
+
+    $descripcionTituladoPasante= $_POST[''];// 2 en la BD( docente-Especializacion)
+    $CURPDatosDocentes= $_POST[''];//3  en la BD(docente-Especializacion)
+    $idEspecializacion = $_POST[''];// 4 en la BD(docente-Especializacion)
+ 
+    
+    $sentencia=" INSERT INTO datosdocente_Especializacion VALUES(        
+    '$descripcionTituladoPasante',
+    '$CURPDatosDocentes',
+    '$idEspecializacion'
+    );";
+
+    if(mysqli_query($conexion,$sentencia)){
+        echo("Escuela agregada correctamente");
+    }
+    else{
+        echo "Error";
+    }
+
+    mysqli_close($conexion);
+?>
