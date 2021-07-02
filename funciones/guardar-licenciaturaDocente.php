32a0be17b39542a96a9890b03557b04be41c45fb
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-01 19:10:20 -0500

    Create guardar-licenciaturaDocente.php

---
+++ funciones/guardar-licenciaturaDocente.php
@@ -0,0 +1,23 @@
+<?php
+    include("funciones/conexion.php");
+
+    $descripcionTituladoPasante= $_POST[''];// 2 en la BD( docente-Licenciatura)
+    $CURPDatosDocentes= $_POST[''];//3  en la BD(docente-Licenciatura)
+    $idLicenciatura = $_POST[''];// 4 en la BD(docente-Licenciatura)
+ 
+    
+    $sentencia=" INSERT INTO datosdocente_Licenciatura VALUES(        
+    '$descripcionTituladoPasante',
+    '$CURPDatosDocentes',
+    '$idLicenciatura'
+    );";
+
+    if(mysqli_query($conexion,$sentencia)){
+        echo("Escuela agregada correctamente");
+    }
+    else{
+        echo "Error";
+    }
+
+    mysqli_close($conexion);
+?>
