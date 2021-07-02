3ca4beb57fb7948316dc80834dc4d47cbfaa0ff9
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-01 19:09:47 -0500

    Create guardar-diplomadoDocente.php

---
+++ funciones/guardar-diplomadoDocente.php
@@ -0,0 +1,23 @@
+<?php
+    include("funciones/conexion.php");
+
+    $descripcionTituladoPasante= $_POST[''];// 2 en la BD( docente-diplomado)
+    $CURPDatosDocentes= $_POST[''];//3  en la BD(docente-diplomado)
+    $idDiplomado = $_POST[''];// 4 en la BD(docente-diplomado)
+ 
+    
+    $sentencia=" INSERT INTO datosdocente_Diplomado VALUES(        
+    '$descripcionTituladoPasante',
+    '$CURPDatosDocentes',
+    '$idDiplomado'
+    );";
+
+    if(mysqli_query($conexion,$sentencia)){
+        echo("Escuela agregada correctamente");
+    }
+    else{
+        echo "Error";
+    }
+
+    mysqli_close($conexion);
+?>
