b307f801d3b17df14b5b234004aa05f7a80d6663
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-03 01:15:30 -0500

    Update guardar-madre.php

--- funciones/guardar-madre.php
+++ funciones/guardar-madre.php
@@ -1,18 +1,18 @@
 <?php
-    include("funciones/conexion.php")
+    include("funciones/conexion.php");
 
-    $nombreMadre= $_POST[];
-    $ape1Madre= $_POST[];
-    $ape2Madre= $_POST[];
-    $telefonoCelularMadre= $_POST[];
-    $CURPMadre= $_POST[];
-    $claveElectroMadre= $_POST[];
-    $idOcupacion= $_POST[];
-    $idNivelEscolar= $_POST[];
-    $idParentesco= $_POST[];
-    $matricula= $_POST[];
+    $nombreMadre= $_POST[''];
+    $ape1Madre= $_POST[''];
+    $ape2Madre= $_POST[''];
+    $telefonoCelularMadre= $_POST[''];
+    $CURPMadre= $_POST[''];
+    $claveElectroMadre= $_POST[''];
+    $idOcupacion= $_POST[''];
+    $idNivelEscolar= $_POST[''];
+    $idParentesco= $_POST[''];
+    $matricula= $_POST[''];
 
-    $sentencia= "INSERT INTO madre VALUES
+    $sentencia= "INSERT INTO madre VALUES (
         '$nombreMadre',
         '$ape1Madre',
         '$ape2Madre',
