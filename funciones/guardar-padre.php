eb197da2084217b055158e2a36a9ca91ed332385
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-03 01:33:08 -0500

    Update guardar-padre.php

--- funciones/guardar-padre.php
+++ funciones/guardar-padre.php
@@ -1,16 +1,16 @@
 <?php
     include("funciones/conexion.php");
 
-    $nombrePadre= $_POST[''];
-    $ape1Padre= $_POST[''];
-    $ape2Padre= $_POST[''];
-    $telefonoCelularPadre= $_POST[''];
-    $CURPPadre= $_POST[''];
-    $claveElectorPadre= $_POST[''];
-    $idOcupacion= $_POST[''];
-    $idNivelEscolar= $_POST[''];
-    $idParentesco= $_POST[''];
-    $matricula= $_POST[''];
+    $nombrePadre= $_POST['txtNombrePadre']; //  en la BD() -- en PHP DatosAlumno(linea  515)
+    $ape1Padre= $_POST['txtApePPadre']; //  en la BD() -- en PHP DatosAlumno(linea 519)
+    $ape2Padre= $_POST['txtApeMPadre']; //  en la BD() -- en PHP DatosAlumno(linea 523 )
+    $telefonoCelularPadre= $_POST['txtTelefonoCelPadre']; //  en la BD() -- en PHP DatosAlumno(linea  559)
+    $CURPPadre= $_POST['txtxCurpPadre']; //  en la BD() -- en PHP DatosAlumno(linea  567)
+    $claveElectorPadre= $_POST['txtClaveElectorPadre']; //  en la BD() -- en PHP DatosAlumno(linea  563)
+    $idOcupacion= $_POST['cmbOcupacionPadre']; //  en la BD() -- en PHP DatosAlumno(linea 543 )
+    $idNivelEscolar= $_POST['cmbUltimoGrado']; //  en la BD() -- en PHP DatosAlumno(linea  571)
+    $idParentesco= $_POST['cmbParentesco']; //  en la BD() -- en PHP DatosAlumno(linea 527)
+    $matricula= $_POST['']; //  en la BD() -- en PHP DatosAlumno(linea  )
 
     $sentencia=" INSERT INTO padre VALUES(
         '$nombrePadre',
