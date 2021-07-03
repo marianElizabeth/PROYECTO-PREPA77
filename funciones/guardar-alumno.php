39683489f80598c76783a7dcb3c6233851b8772e
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-03 01:11:46 -0500

    Update guardar-alumno.php

--- funciones/guardar-alumno.php
+++ funciones/guardar-alumno.php
@@ -1,62 +1,64 @@
 <?php
-    include("funciones/conexion.php");
+    include("conexion.php");
 
-    $matricula= $_POST['txtMatriculaAlum'];//1  en la BD(datosAlumono)
-    $nombreAlumno= $_POST['txtNombreAlum'];// 2 en la BD(datosAlumono)
-    $ape1Alumno= $_POST['txtApePAlum'];// 3 en la BD(datosAlumono)
-    $ape2Alumno= $_POST['txtApeMAlum'];// 4 en la BD(datosAlumono)
-    $fechaBaja= $_POST['fechaBajaAlum'];// 5 en la BD(datosAlumono)
-    $numExpediente= $_POST['txtNumExpedienteAlum'];//6  en la BD(datosAlumono)
-    $folioAlumno= $_POST['txtFolioAlum'];//7  en la BD(datosAlumono)
-    $CURPAlumno= $_POST['txtCurpAlum'];// 8 en la BD(datosAlumono)
-    $fechaNacimientoAlumno= $_POST['fechaNacimientoAlum'];// 9 en la BD(datosAlumono)
-    $correoPersonalAlumno= $_POST['txtCorreoPersonalAlum'];// 10 en la BD(datosAlumono)
-    $correoInstitucionalAlumno= $_POST['txtCorreoInstitucionalAlum'];// 11 en la BD(datosAlumono)
-    $referenciaDomicilio= $_POST['txtReferenciaAlum'];//12 en la BD(datosAlumono)
-    $telefonoFijoAlumno= $_POST['txtTelefonoFijoAlum'];//13 en la BD(datosAlumono)
-    $telefonoCelularAlumno= $_POST['txtTelefonoCelAlum'];//14 en la BD(datosAlumono)
-    $numSeguroSocial= $_POST['txtNumSeguroSocialAlum'];//15 en la BD(datosAlumono)
-    $folioBeca= $_POST['txtFolioBecaAlum'];//16 en la BD(datosAlumono)    
-    $aciertosExamenIngresoAlumno= $_POST['txtAciertosExamenIngresoAlum'];//17 en la BD(datosAlumono)
-    $promSecundaria= $_POST['txtPromedioEgresoSecundariaAlum'];//18 en la BD(datosAlumono)
-    $nombreTutor= $_POST['txtNombreTutorAlum'];//19 en la BD(datosAlumono)
+    $matricula= $_POST['txtMatriculaAlum'];//1  en la BD(datosAlumono) -- en PHP(linea 141)
+    $nombreAlumno= $_POST['txtNombreAlum'];// 2 en la BD(datosAlumono)-- en PHP(linea 214 )
+    $ape1Alumno= $_POST['txtApePAlum'];// 3 en la BD(datosAlumono)-- en PHP(linea 218)
+    $ape2Alumno= $_POST['txtApeMAlum'];// 4 en la BD(datosAlumono)-- en PHP(linea 222)
+    $fechaBaja= $_POST['fechaBajaAlum'];// 5 en la BD(datosAlumono)-- en PHP(linea 408 )
+    $numExpediente= $_POST['txtNumExpedienteAlum'];//6  en la BD(datosAlumono) -- en PHP(linea 145)
+    $folioAlumno= $_POST['txtFolioAlum'];//7  en la BD(datosAlumono)-- en PHP(linea 149)
+    $CURPAlumno= $_POST['txtCurpAlum'];// 8 en la BD(datosAlumono)-- en PHP(linea 202)
+    $fechaNacimientoAlumno= $_POST['fechaNacimientoAlum'];// 9 en la BD(datosAlumono) -- en PHP(linea 259 )
+    $correoPersonalAlumno= $_POST['txtCorreoPersonalAlum'];// 10 en la BD(datosAlumono) -- en PHP(linea 332)
+    $correoInstitucionalAlumno= $_POST['txtCorreoInstitucionalAlum'];// 11 en la BD(datosAlumono) -- en PHP(linea 336)
+    $referenciaDomicilio= $_POST['txtReferenciaAlum'];//12 en la BD(datosAlumono) -- en PHP(linea 508)
+    $telefonoFijoAlumno= $_POST['txtTelefonoFijoAlum'];//13 en la BD(datosAlumono) -- en PHP(linea 287)
+    $telefonoCelularAlumno= $_POST['txtTelefonoCelAlum'];//14 en la BD(datosAlumono)-- en PHP(linea 283)
+    $numSeguroSocial= $_POST['txtNumSeguroSocialAlum'];//15 en la BD(datosAlumono) -- en PHP(linea 327)
+    $folioBeca= $_POST['txtFolioBecaAlum'];//16 en la BD(datosAlumono)    -- en PHP(linea 372)
+    $aciertosExamenIngresoAlumno= $_POST['txtAciertosExamenIngresoAlum'];//17 en la BD(datosAlumono) -- en PHP(linea 418 )
+    $promSecundaria= $_POST['txtPromedioEgresoSecundariaAlum'];//18 en la BD(datosAlumono)-- en PHP(linea 440)
+    $nombreTutor= $_POST['txtNombreTutorAlum'];//19 en la BD(datosAlumono) -- en PHP(linea 291)
 
-    $idSexo= $_POST['cmbSexoAlum']; //20 en la BD(datosAlumono)
+    $idSexo= $_POST['cmbSexoAlum']; //20 en la BD(datosAlumono) -- en PHP(linea 226 )
     
-    $idEdad= $_POST['txtEdadAlum']; //21 en la BD(datosAlumono)
+    $idEdad= $_POST['cmbEdadAlum']; //21 en la BD(datosAlumono) -- en PHP(linea 242)
 
-    $idGrupo= $_POST['txtGrupoAlum'];//22 en la BD(datosAlumono)
+    $idGrupo= $_POST['cmbGrupoAlum'];//22 en la BD(datosAlumono) -- en PHP(linea  185)
 
-    $idGrado= $_POST['cmbGradoAlum'];//23 en la BD(datosAlumono)
+    $idGrado= $_POST['cmbGradoAlum'];//23 en la BD(datosAlumono) -- en PHP(linea 169)
 
-    $idLocalidad= $_POST['cmbLocalidadAlum'];// 24 en la BD(datosAlumono)
+    $idLocalidad= $_POST['cmbLocalidadAlum'];// 24 en la BD(datosAlumono) -- en PHP(linea 466)
 
-    $idAlumnoTrabaja= $_POST['cmbTrabajaAlum'];// 25  en la BD(datosAlumono)
+    $idAlumnoTrabaja= $_POST['cmbTrabajaAlum'];// 25  en la BD(datosAlumono) -- en PHP(linea 340)
 
-    $idTipoSeguroSocial= $_POST['cmbTipoSocialAlum'];// 26  en la BD(datosAlumono)
+    $idTipoSeguroSocial= $_POST['cmbTipoSocialAlum'];// 26  en la BD(datosAlumono) -- en PHP(linea 311)
 
-    $idInscritoAlumnno= $_POST['cmbInscritoAlum'];//27  en la BD(datosAlumono)
+    $idInscritoAlumnno= $_POST['cmbInscritoAlum'];//27  en la BD(datosAlumono) -- en PHP(linea 153 )
     
-    $idParentesco= $_POST['cmbParentescoTutorAlum'];// 28 en la BD(datosAlumono)
+    $idParentesco= $_POST['cmbDependeEconomicamenteAlum'];// 28 en la BD(datosAlumono) -- en PHP(linea 376) -- la opcion de parentesco del tutor no esta en la base de datos linea 295
+     
+    $idBeca= $_POST['cmbBecaAlum'];//29  en la BD(datosAlumono) -- en PHP(linea 356 )
+
+    $idEntidadFederativa= $_POST['cmbEntidadFederativaAlum'];// 30 en la BD(datosAlumono) -- en PHP(linea 267)
+
+    $CCTEscuela= $_POST['cmbSecundariaProcedenciaAlum'];//31  en la BD(datosAlumono) -- en PHP(linea 423 )
+
+    $numDomicilio= $_POST['txtNumeroAlum'];//32  en la BD(datosAlumono) -- en PHP(linea 453)
+    $calleDomicilio= $_POST['txtCalleAlum'];//33  en la BD(datosAlumono)    -- en PHP(linea 448 )
+    $colonia= $_POST['txtColoniaAlum'];//34  en la BD(datosAlumono) -- en PHP(linea 458)
+    $CP= $_POST['txtCodiopostalAlum'];//35  en la BD(datosAlumono) -- en PHP(linea 462)
+
+    $idOcupacion= $_POST['cmbOcupacionDependeEconomicamenteAlum'];//36  en la BD(datosAlumono) -- en PHP(linea 392 )
+
+    $folioCURPAlumno= $_POST['txtFolioCurpAlum'];//37  en la BD(datosAlumono) -- en PHP(linea 206 )
+    $RFCAlumno= $_POST['txtRFCAlum'];//38  en la BD(datosAlumono) -- en PHP(linea  210)
+
+    $municipioNacimientoAlumno= $_POST['txtMunicipioNacimientoAlum'];//39  en la BD(datosAlumono) -- en PHP(linea 263)
     
-    $idBeca= $_POST['cmbBecaAlum'];//29  en la BD(datosAlumono)
+    /**SOLO SE AGEGARON HASTA LA LINEA  508 DEL PHP(DATOS ALUMNO)*/
 
-    $idEntidadFederativa= $_POST['cmbEstadoNacimientoAlum'];// 30 en la BD(datosAlumono)
-
-    $CCTEscuela= $_POST['txtSecundariaProcedenciaAlum'];//31  en la BD(datosAlumono)
-
-    $numDomicilio= $_POST['txtNumeroAlum'];//32  en la BD(datosAlumono)
-    $calleDomicilio= $_POST['txtCalleAlum'];//33  en la BD(datosAlumono)    
-    $colonia= $_POST['txtColoniaAlum'];//34  en la BD(datosAlumono)
-    $CP= $_POST['txtCodiopostalAlum'];//35  en la BD(datosAlumono)
-
-    $idOcupacion= $_POST['cmbOcupacionDependeEconomicamenteAlum'];//36  en la BD(datosAlumono)
-
-    $folioCURPAlumno= $_POST['txtFolioCurpAlum'];//37  en la BD(datosAlumono)
-    $RFCAlumno= $_POST['txtRFCAlum'];//38  en la BD(datosAlumono)
-
-    $municipioNacimientoAlumno= $_POST['txtMunicipioNacimientoAlumno'];//38  en la BD(datosAlumono)
-    
     //$= $_POST['PROCEDENDCIA LINEA 317 -- DATOS GENERALES ALUMNO'];    
     //DATOS DE LA DIRECCIÓN    
     //$= $_POST['txtColoniaAlum'];//  en la BD(datosAlumono)
@@ -64,7 +66,7 @@
     //$= $_POST['cmbMunicipio'];//  en la BD(datosAlumono)
     //$= $_POST[''];
     
-    $sentencia=" INSERT INTO datosAlumno VALUES(
+    $sentencia = " INSERT INTO datosAlumno VALUES(
         '$matricula',
         '$nombreAlumno',
         '$ape1Alumno',
@@ -73,7 +75,7 @@
         '$numExpediente',
         '$folioAlumno',
         '$CURPAlumno',
-         $fechaNacimientoAlumno,
+        '$fechaNacimientoAlumno',
         '$correoPersonalAlumno',
         '$correoInstitucionalAlumno',
         '$referenciaDomicilio',
@@ -90,19 +92,20 @@
         $idGrado,
         $idLocalidad,
         $idAlumnoTrabaja,
-        $idTipoSeguroSocial',
-        $idInscritoAlumnno',    
+        $idTipoSeguroSocial,
+        $idInscritoAlumnno,    
         $idParentesco,
         $idBeca,
-        $idMunicipio,
+        $idEntidadFederativa,
         '$CCTEscuela',
         '$numDomicilio',
         '$calleDomicilio',
         '$colonia',
         '$CP',
-        '$idOcupacion,
+        $idOcupacion,
         '$folioCURPAlumno',
-        '$RFCAlumno'
+        '$RFCAlumno',
+        '$municipioNacimientoAlumno'
     );";
 
     if(mysqli_query($conexion,$sentencia)){
