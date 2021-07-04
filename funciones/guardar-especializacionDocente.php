a8de6d41f5e92794e29307ba63e24831ab37df1b
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-04 16:23:23 -0500

    Update guardar-especializacionDocente.php

--- funciones/guardar-especializacionDocente.php
+++ funciones/guardar-especializacionDocente.php
@@ -1,18 +1,21 @@
 <?php
     include("funciones/conexion.php");
 
-    $descripcionTituladoPasante= $_POST[''];// 2 en la BD( docente-Especializacion)
-    $CURPDatosDocentes= $_POST[''];//3  en la BD(docente-Especializacion)
-    $idEspecializacion = $_POST[''];// 4 en la BD(docente-Especializacion)
+    //INSERTAR DATOS EN ESPECIALIZACION
+    //$descripcionTituladoPasanteEspecializacion= $_POST[''];// 2 en la BD( docente-Especializacion) ---PHP LINEA
+    $CURPDatosDocentes= $_POST['txtCURPDocente'];//3  en la BD(docente-Especializacion) ---PHP LINEA
+    $idEspecializacion = $_POST['cmbEspecializacion'];// 4 en la BD(docente-Especializacion) ---PHP LINEA 353
+    $fechaEgresoEspecializacion= $_POST['fechaEgresoEspecializacion'];//12  en la BD(datosDocente) -- en PHP guardar-datosgeneralesdocente(linea 369  )
  
     
-    $sentencia=" INSERT INTO datosdocente_Especializacion VALUES(        
-    '$descripcionTituladoPasante',
+    $sentenciaEspecializacion=" INSERT INTO datosdocente_especializacion VALUES(        
+    DEFAULT,
     '$CURPDatosDocentes',
-    '$idEspecializacion'
+    $idEspecializacion,
+    '$fechaEgresoEspecializacion'
     );";
 
-    if(mysqli_query($conexion,$sentencia)){
+    if(mysqli_query($conexion,$sentenciaEspecializacion)){
         echo("Especializacion agregada correctamente");
     }
     else{
