d279d612c8dfb90aab2fe604febc5b15749d5b57
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-04 17:29:58 -0500

    Update guardar-maestriaDocente.php

--- funciones/guardar-maestriaDocente.php
+++ funciones/guardar-maestriaDocente.php
@@ -2,7 +2,7 @@
     include("conexion.php");
     //MAESTRIA
     $descripcionTituladoPasanteMaestria= $_POST['cmbEstatusMaestria'];// 2 en la BD( docente-Maestria)
-    $CURPDatosDocentes= $_POST[''];//3  en la BD(docente-Maestria)
+    $CURPDatosDocentes= $_POST['txtCURPDocente'];//3  en la BD(docente-Maestria)
     $idMaestria = $_POST['cmbMaestria'];// 4 en la BD(docente-Maestria)
     $noCedulaProfesionalMaestria= $_POST['txtNoCedulaMaestria'];// 14 en la BD(datosDocente) -- en PHP guardar-datosgeneralesdocente(linea  )    
  
