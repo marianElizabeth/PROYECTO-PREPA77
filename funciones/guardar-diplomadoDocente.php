2a5b1fd2dc9af708d34373acdf6956bf2ead4d59
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-04 16:12:02 -0500

    Update guardar-diplomadoDocente.php

--- funciones/guardar-diplomadoDocente.php
+++ funciones/guardar-diplomadoDocente.php
@@ -1,18 +1,21 @@
 <?php
     include("funciones/conexion.php");
 
-    $descripcionTituladoPasante= $_POST[''];// 2 en la BD( docente-diplomado)
-    $CURPDatosDocentes= $_POST[''];//3  en la BD(docente-diplomado)
-    $idDiplomado = $_POST[''];// 4 en la BD(docente-diplomado)
- 
-    
-    $sentencia=" INSERT INTO datosdocente_Diplomado VALUES(        
-    '$descripcionTituladoPasante',
-    '$CURPDatosDocentes',
-    '$idDiplomado'
+    //DIPLOMADO 
+    //$descripcionTituladoPasante= $_POST[''];// 2 en la BD( docente-diplomado)
+
+    $CURPDatosDocentes= $_POST['txtCURPDocente'];//3  en la BD(docente-diplomado)
+    $idDiplomado = $_POST['cmbDiplomado'];// 4 en la BD(docente-diplomado)---php 373
+    $fechaEgresoDiplomado= $_POST['fechaEgresoDiplomado'];// 13 en la BD(datosDocente)   -- en PHP guardar-datosgeneralesdocente(linea  )
+
+    $sentenciaDiplomado=" INSERT INTO datosdocentes_diplomado VALUES(        
+    DEFAULT,
+    '$CURPDocente',
+    $idDiplomado,
+    '$fechaEgresoDiplomado'
     );";
 
-    if(mysqli_query($conexion,$sentencia)){
+    if(mysqli_query($conexion,$sentenciaDiplomado)){
         echo("Diplomado agregada correctamente");
     }
     else{
