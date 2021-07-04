5abec01404d82d4ef4f0947450a9b91db184c2ad
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-04 18:26:41 -0500

    Update guardar-escuelanumero.php

---
+++ funciones/guardar-escuelanumero.php
@@ -0,0 +1,27 @@
+<?php
+
+    include("conexion.php");
+
+    $numPlaza= $_POST['txtNoPlaza'];//23 en la BD(datosDocente)
+    $numPrelacion= $_POST['txtNumPrelacion'];// 24  en la BD(datosDocente)
+    $numHoras = $_POST['txtNumHoras'];// 24  en la BD(datosDocente)
+    $CCTEscuela = $_POST['txtEscuelaAsignacion'];// 24  en la BD(datosDocente)
+    $CURPDocente = $_POST['txtCURPDocente'];// 24  en la BD(datosDocente)    
+
+    $sentencia=" INSERT INTO escuelasnumeros VALUES(        
+        DEFAULT,
+        '$numPlaza',
+        '$numPrelacion',  
+        '$numHoras',
+        '$CCTEscuela',
+        '$CURPDocente'
+    );";
+    
+    if(mysqli_query($conexion,$sentencia)){
+        echo("ESCUELA CON NUMEROS Almacenado Correctamene");
+    }
+    else{
+        echo "Error";
+    }
+
+?>
