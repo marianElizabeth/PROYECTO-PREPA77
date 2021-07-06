5e1e6d126ecea8aa882406ae798644383caf93a4
RodrigoJM <108javierr@gmail.com>
2021-07-05 20:33:26 -0500

    Modificaciones para borrar

--- funciones/eliminar-maestria.php
+++ funciones/eliminar-maestria.php
@@ -3,94 +3,17 @@
     
     $CURPDocente=$_GET['CurpDelete'];
 
-    $sentencia1= "
-    DELETE FROM escuelasnumeros WHERE CURPDocente = '$CURPDocente';
-    ;
-    ";
-    if(mysqli_query($conexion,$sentencia1)){
-        header("Location: ../VerMaestriaDocente.php");    
-    }
-    else{
-        echo "Error";
-    }
-
-    $sentencia2= "
-
-    DELETE FROM  escuelaslabora WHERE CURPDocente = '$CURPDocente';
-    ;
-    ";
-    if(mysqli_query($conexion,$sentencia2)){
-        header("Location: ../VerMaestriaDocente.php");    
-    }
-    else{
-        echo "Error";
-    }
-
-    $sentencia3= "
-    DELETE FROM datosdocentes_diplomado WHERE CURPDatosDocente = '$CURPDocente';
-    ;
-    ";
-    if(mysqli_query($conexion,$sentencia3)){
-        header("Location: ../VerMaestriaDocente.php");    
-    }
-    else{
-        echo "Error";
-    }
-
-    $sentencia4= "
+    $sentencia= "
 
     DELETE FROM datosdocente_maestria WHERE CURPDatosDocente = '$CURPDocente';
     ;
     ";
-    if(mysqli_query($conexion,$sentencia4)){
+    if(mysqli_query($conexion,$sentencia)){
         header("Location: ../VerMaestriaDocente.php");    
     }
     else{
         echo "Error";
     }
 
-    $sentencia5= "
-        DELETE FROM  datosdocente_licenciatura WHERE CURPDatosDocente = '$CURPDocente';
-    ;
-    ";
-    if(mysqli_query($conexion,$sentencia5)){
-        header("Location: ../VerMaestriaDocente.php");    
-    }
-    else{
-        echo "Error";
-    }
-
-    $sentencia6= "
-        DELETE FROM  datosdocente_especializacion WHERE CURPDatosDocentes = '$CURPDocente';
-    ;
-    ";
-    if(mysqli_query($conexion,$sentencia6)){
-        header("Location: ../VerMaestriaDocente.php");    
-    }
-    else{
-        echo "Error";
-    }
-
-    $sentencia7= "
-    DELETE FROM  datosdocente_doctorado WHERE CURPDatosDocentes = '$CURPDocente';
-    ;
-    ";
-    if(mysqli_query($conexion,$sentencia7)){
-        header("Location: ../VerMaestriaDocente.php");    
-    }
-    else{
-        echo "Error";
-    }
-
-    $sentencia8= "
-        DELETE FROM datosdocentes WHERE CURPDocente = '$CURPDocente';
-    ;
-    ";
-    if(mysqli_query($conexion,$sentencia8)){
-        header("Location: ../VerMaestriaDocente.php");    
-    }
-    else{
-        echo "Error";
-    }
     mysqli_close($conexion);
 ?>
