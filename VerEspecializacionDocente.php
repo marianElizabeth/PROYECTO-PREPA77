a66927e1ae70248e907fdd853d3d08fd47509045
RodrigoJM <108javierr@gmail.com>
2021-07-05 19:26:12 -0500

    Modificaciones para borrar

--- VerEspecializacionDocente.php
+++ VerEspecializacionDocente.php
@@ -240,7 +240,7 @@
 										  <td>".$registro["fechaEgresoEspecializacion"]."</td>
 										  
 											<td><a href='#!' class='btn btn-success btn-raised btn-xs'><i class='zmdi zmdi-refresh'></i></a></td>
-											<td><a href='#!' class='btn btn-danger btn-raised btn-xs'><i class='zmdi zmdi-delete'></i></a></td>
+											<td><a href='funciones/eliminar-especializacion.php?CurpDelete=".$registro["CURPDatosDocentes"]."' class='btn btn-danger btn-raised btn-xs'><i class='zmdi zmdi-delete'></i></a></td>
 											</tr>
 											";
 									}
---
+++ funciones/eliminar-especializacion.php
@@ -0,0 +1,96 @@
+<?php
+    include("conexion.php");
+    
+    $CURPDocente=$_GET['CurpDelete'];
+
+    $sentencia1= "
+    DELETE FROM escuelasnumeros WHERE CURPDocente = '$CURPDocente';
+    ;
+    ";
+    if(mysqli_query($conexion,$sentencia1)){
+        header("Location: ../VerEspecializacionDocente.php");    
+    }
+    else{
+        echo "Error";
+    }
+
+    $sentencia2= "
+
+    DELETE FROM  escuelaslabora WHERE CURPDocente = '$CURPDocente';
+    ;
+    ";
+    if(mysqli_query($conexion,$sentencia2)){
+        header("Location: ../VerEspecializacionDocente.php");    
+    }
+    else{
+        echo "Error";
+    }
+
+    $sentencia3= "
+    DELETE FROM datosdocentes_diplomado WHERE CURPDatosDocente = '$CURPDocente';
+    ;
+    ";
+    if(mysqli_query($conexion,$sentencia3)){
+        header("Location: ../VerEspecializacionDocente.php");    
+    }
+    else{
+        echo "Error";
+    }
+
+    $sentencia4= "
+
+    DELETE FROM datosdocente_maestria WHERE CURPDatosDocente = '$CURPDocente';
+    ;
+    ";
+    if(mysqli_query($conexion,$sentencia4)){
+        header("Location: ../VerEspecializacionDocente.php");    
+    }
+    else{
+        echo "Error";
+    }
+
+    $sentencia5= "
+        DELETE FROM  datosdocente_licenciatura WHERE CURPDatosDocente = '$CURPDocente';
+    ;
+    ";
+    if(mysqli_query($conexion,$sentencia5)){
+        header("Location: ../VerEspecializacionDocente.php");    
+    }
+    else{
+        echo "Error";
+    }
+
+    $sentencia6= "
+        DELETE FROM  datosdocente_especializacion WHERE CURPDatosDocentes = '$CURPDocente';
+    ;
+    ";
+    if(mysqli_query($conexion,$sentencia6)){
+        header("Location: ../VerEspecializacionDocente.php");    
+    }
+    else{
+        echo "Error";
+    }
+
+    $sentencia7= "
+    DELETE FROM  datosdocente_doctorado WHERE CURPDatosDocentes = '$CURPDocente';
+    ;
+    ";
+    if(mysqli_query($conexion,$sentencia7)){
+        header("Location: ../VerEspecializacionDocente.php");    
+    }
+    else{
+        echo "Error";
+    }
+
+    $sentencia8= "
+        DELETE FROM datosdocentes WHERE CURPDocente = '$CURPDocente';
+    ;
+    ";
+    if(mysqli_query($conexion,$sentencia8)){
+        header("Location: ../VerEspecializacionDocente.php");    
+    }
+    else{
+        echo "Error";
+    }
+    mysqli_close($conexion);
+?>
