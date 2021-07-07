fdca2757c81119e072961779228e9ba071a3a3ce
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-06 23:14:19 -0500

    Create actualizar-especializaciondocente.php

---
+++ funciones/actualizar-especializaciondocente.php
@@ -0,0 +1,21 @@
+<?php 
+    include("conexion.php");
+    //ESPECIALIZACION
+    $CURPDatosDocentes= $_POST['txtCURPDocente'];//3  en la BD(docente-Especializacion) ---PHP LINEA
+    $idEspecializacion = $_POST['cmbEspecializacion'];// 4 en la BD(docente-Especializacion) ---PHP LINEA 353
+    $fechaEgresoEspecializacion= $_POST['fechaEgresoEspecializacion'];//12  en la BD(datosDocente) -- en PHP guardar-datosgeneralesdocente(linea 369  )
+ 
+    
+    $sentenciaEspecializacion=" UPDATE datosdocente_especializacion SET
+        idEspecializacion=(SELECT idEspecializacion FROM especializacion WHERE descripcionEspecializacion = '$idEspecializacion'),
+        fechaEgresoEspecializacion='$fechaEgresoEspecializacion'
+        WHERE CURPDatosDocentes='$CURPDatosDocentes';";  
+
+    if(mysqli_query($conexion,$sentenciaEspecializacion)){
+        echo("Especializacion actualizada correctamente");
+    }
+    else{
+        echo "Error";
+    }
+    mysqli_close($conexion);
+?>
