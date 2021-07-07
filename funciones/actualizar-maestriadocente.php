255dfcdca1f965b294b138bcd3ebe05875343ba4
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-06 23:15:05 -0500

    Create actualizar-maestriadocente.php

---
+++ funciones/actualizar-maestriadocente.php
@@ -0,0 +1,23 @@
+<?php 
+    include("conexion.php");
+    //MAESTRIA
+    $descripcionTituladoPasanteMaestria= $_POST['cmbEstatusMaestria'];// 2 en la BD( docente-Maestria)
+    $CURPDatosDocentes= $_POST['txtCURPDocente'];//3  en la BD(docente-Maestria)
+    $idMaestria = $_POST['cmbMaestria'];// 4 en la BD(docente-Maestria)
+    $noCedulaProfesionalMaestria= $_POST['txtNoCedulaMaestria'];// 14 en la BD(datosDocente) -- en PHP guardar-datosgeneralesdocente(linea  )    
+ 
+    
+    $sentenciaMaestria="  UPDATE datosdocente_maestria SET
+    descripcionTituladoPasante ='$descripcionTituladoPasanteMaestria',
+    idMaestria = (SELECT idMaestria FROM maestria WHERE descripcionMaestria = '$idMaestria'),
+    noCedulaProfesionalMaestria ='$noCedulaProfesionalMaestria'
+    WHERE CURPDatosDocente='$CURPDatosDocentes';";
+
+    if(mysqli_query($conexion,$sentenciaMaestria)){
+        echo("Maestria actualizada correctamente");
+    }
+    else{
+        echo "Error";
+    }
+    mysqli_close($conexion);
+?>
