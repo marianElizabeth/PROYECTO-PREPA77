75bdcc92344dd5af9def31539532aaf6793cffcb
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-06 23:12:28 -0500

    Update actualizar-dilpomadodocente.php

--- funciones/actualizar-dilpomadodocente.php
+++ funciones/actualizar-dilpomadodocente.php
@@ -6,7 +6,7 @@
     $fechaEgresoDiplomado= $_POST['fechaEgresoDiplomado'];// 13 en la BD(datosDocente)   -- en PHP guardar-datosgeneralesdocente(linea  )
 
     $sentenciaDiplomado=" UPDATE datosdocentes_diplomado SET         
-        idDiplomado = $idDiplomado (SELECT idDiplomado FROM diplomado WHERE descripcionDiplomado = '$idDiplomado'),
+        idDiplomado =(SELECT idDiplomado FROM diplomado WHERE descripcionDiplomado = '$idDiplomado'),
         fechaEgresoDiplomado='$fechaEgresoDiplomado'
         WHERE CURPDatosDocente='$CURPDatosDocentes';";
 
