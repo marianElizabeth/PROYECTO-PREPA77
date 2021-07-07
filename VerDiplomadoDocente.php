3caaf403b5df3a679ae8af8f77920fad0e85203d
marianElizabeth <61371595+marianElizabeth@users.noreply.github.com>
2021-07-06 23:53:46 -0500

    Update VerDiplomadoDocente.php

--- VerDiplomadoDocente.php
+++ VerDiplomadoDocente.php
@@ -234,13 +234,23 @@
 										  <td>".$registro["fechaEgresoDiplomado"]."</td>
 										  
 											<td><a href='actualizar-diplomado.php?CurpUpdate=".$registro["CURPDatosDocente"]."' class='btn btn-success btn-raised btn-xs'><i class='zmdi zmdi-refresh'></i></a></td>
-											<td><a href='funciones/eliminar-diplomado.php?CurpDelete=".$registro["CURPDatosDocente"]."' class='btn btn-danger btn-raised btn-xs'><i class='zmdi zmdi-delete'></i></a></td>
+											<td><a href='funciones/eliminar-diplomado.php?CurpDelete=".$registro["CURPDatosDocente"]."'><button class='btn btn-danger btn-raised btn-xs' type='button' onclick='return confirmarEliminar()'><i class='zmdi zmdi-delete'></i></button></a></td>
 											</tr>
 											";
-									}
-									
-										mysqli_close($conexion);
-									  ?>
+										  }																										 
+										  mysqli_close($conexion);
+										?>
+										<script type="text/javascript">
+											function confirmarEliminar(){
+												var respuesta = confirm("Esta seguro de Eliminar");
+												if(respuesta==false){
+													return false;
+												}
+												else{
+													return true;
+												}
+											}
+										</script>
 									</tbody>
 								</table>
 							</div>
