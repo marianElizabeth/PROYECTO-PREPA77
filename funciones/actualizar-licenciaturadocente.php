041548bb6399fcf2e8765726f90a59fc0c268876
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-06 23:14:36 -0500

    Create actualizar-licenciaturadocente.php

---
+++ funciones/actualizar-licenciaturadocente.php
@@ -0,0 +1,27 @@
+<?php 
+    include("conexion.php");
+    //LICENCIATURE
+    $descripcionTituladoPasanteLicenciatura= $_POST['cmbEstatusDocente'];// 2 en la BD( docente-Licenciatura)---php linea 333
+    $CURPDatosDocentes= $_POST['txtCURPDocente'];//3  en la BD(docente-Licenciatura) ---php linea
+    $idLicenciatura = $_POST['txtLicDocente'];// 4 en la BD(docente-Licenciatura)---php linea 317
+    $escuelaEgresionLicenciatura = $_POST['txtEscEgresion'];// 5 en la BD(docente-Licenciatura) ---php linea 345
+    $noCedulaProfesional= $_POST['txtNoCedulaProfDocente'];// 10 en la BD(datosDocente) -- en PHP guardar-datosgeneralesdocente(linea  341)
+    $fechaEscuelaEgresion= $_POST['fechaEgresion'];//11 en la BD(datosDocente)  -- en PHP guardar-datosgeneralesdocente(linea  349)
+ 
+    
+    $sentenciaLicenciature=" UPDATE datosdocente_licenciatura SET
+        descripcionTituladoPasante='$descripcionTituladoPasanteLicenciatura',
+        idLicenciatura=(SELECT idLicenciatura FROM licenciatura WHERE descripcionLicenciatura = '$idLicenciatura'),
+        escuelaEgresionLicenciatura= '$escuelaEgresionLicenciatura'
+        noCedulaProfesional=$noCedulaProfesional,
+        fechaEscuelaEgresion='$fechaEscuelaEgresion'
+        WHERE CURPDatosDocente='$CURPDatosDocentes';";  
+
+    if(mysqli_query($conexion,$sentenciaLicenciature)){
+        echo("Licenciatura actualizado correctamente");
+    }
+    else{
+        echo "Error";
+    }
+    mysqli_close($conexion);
+?>
