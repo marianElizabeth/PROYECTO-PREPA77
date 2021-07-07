491b1af3e0827547fa92a0b58e534fce136e7f1e
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-06 23:13:58 -0500

    Create actualizar-escuelasnumeros.php

---
+++ funciones/actualizar-escuelasnumeros.php
@@ -0,0 +1,24 @@
+<?php 
+    include("conexion.php");
+    //ESCUELASNUMEROS
+    $numPlaza= $_POST['txtNoPlaza'];//23 en la BD(datosDocente)
+    $numPrelacion= $_POST['txtNumPrelacion'];// 24  en la BD(datosDocente)
+    $numHoras = $_POST['txtNumHoras'];// 24  en la BD(datosDocente)
+    $CCTEscuela = $_POST['txtEscuelaAsignacion'];
+    $CURPDocente = $_POST['txtCURPDocente'];// 24  en la BD(datosDocente)    
+
+    $sentencia2=" UPDATE escuelasnumeros SET
+        numPlaza='$numPlaza',
+        numPrelacion='$numPrelacion',  
+        numHoras='$numHoras',
+        CCTEscuela= (SELECT CCTEscuela FROM escuela WHERE nombreEscuela = '$CCTEscuela')
+        WHERE CURPDocente='$CURPDocente';";    
+    
+    if(mysqli_query($conexion,$sentencia2)){
+        echo("ESCUELA CON NUMEROS Actualizada Correctamene");
+    }
+    else{
+        echo "Error";
+    }
+    mysqli_close($conexion);
+?>
