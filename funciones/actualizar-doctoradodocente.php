382a5c18c7c995408293816d7562a62facbcd6c0
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-06 23:11:37 -0500

    Update actualizar-doctoradodocente.php

--- funciones/actualizar-doctoradodocente.php
+++ funciones/actualizar-doctoradodocente.php
@@ -1,5 +1,23 @@
 <?php 
     include("conexion.php");
+    //DOCTORADE
+    $descripcionTituladoPasante= $_POST['cmbEstatusDoctorado'];// 2 en la BD( docente-doctorado)
+    $CURPDatosDocentes= $_POST['txtCURPDocente'];//3  en la BD(docente-doctorado)
+    $idDoctorado = $_POST['cmbDoctorado'];// 4 en la BD(docente-doctorado)
+    $noCedulaProfesionalDoctorado= $_POST['txtNCedulaDoctorado'];//15 en la BD(dato) -- en PHP guardar-datosgeneralesdocente(linea  )
+ 
     
+    $sentenciaDoctorado=" UPDATE datosdocente_doctorado SET        
+        descripcionTituladoPasante='$descripcionTituladoPasante',
+        idDoctorado = (SELECT idDoctorado FROM doctorado WHERE descripcionDoctorado = '$idDoctorado'),
+        noCedulaProfesionalDoctorado='$noCedulaProfesionalDoctorado'
+        WHERE CURPDatosDocentes='$CURPDatosDocentes';";
+    
+        if(mysqli_query($conexion,$sentenciaDoctorado)){
+            echo("Doctorado actualizado correctamente");
+        }
+        else{
+            echo "Error";
+        }
     mysqli_close($conexion);
 ?>
