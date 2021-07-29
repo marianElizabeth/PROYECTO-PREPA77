b262ffd1dca80cae656a9933f116fdc4b0792bd4
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-29 01:59:25 -0500

    Create calcuartosemestre.php

---
+++ funciones/calcuartosemestre.php
@@ -0,0 +1,182 @@
+<?php
+    include("conexion.php");
+    $matricula= $_POST['txtMatriculaAlum'];    
+
+    $txtCalMateIV_1= $_POST['txtCalMateIV_1'];
+    $txtCalMateIV_2= $_POST['txtCalMateIV_2'];
+    $txtCalMateIV_3= $_POST['txtCalMateIV_3'];
+    $txtFaltaMateIV_1= $_POST['txtFaltaMateIV_1'];
+    $txtFaltaMateIV_2= $_POST['txtFaltaMateIV_2'];
+    $txtFaltaMateIV_3= $_POST['txtFaltaMateIV_3'];
+    $txtCalExtraMateIV_1 = $_POST['txtCalExtraMateIV_1'];
+    $txtFechaCalExtraMateIV_1 = $_POST['txtFechaCalExtraMateIV_1'];
+    $txtCalExtraMateIV_2 = $_POST['txtCalExtraMateIV_2'];
+    $txtFechaCalExtraMateIV_2 = $_POST['txtFechaCalExtraMateIV_2'];
+    $txtCalExtraMateIV_3 = $_POST['txtCalExtraMateIV_3'];
+    $txtFechaCalExtraMateIV_3 = $_POST['txtFechaCalExtraMateIV_3'];
+    $txtCalExtraMateIV_4 = $_POST['txtCalExtraMateIV_4'];
+    $txtFechaCalExtraMateIV_4 = $_POST['txtFechaCalExtraMateIV_4'];
+
+    $txtCalBiologiaII_1= $_POST['txtCalBiologiaII_1'];
+    $txtCalBiologiaII_2= $_POST['txtCalBiologiaII_2'];
+    $txtCalBiologiaII_3= $_POST['txtCalBiologiaII_3'];
+    $txtFaltaBiologiaII_1 = $_POST['txtFaltaBiologiaII_1'];
+    $txtFaltaBiologiaII_2 = $_POST['txtFaltaBiologiaII_2'];
+    $txtFaltaBiologiaII_3 = $_POST['txtFaltaBiologiaII_3'];
+    $txtCalExtraBiologiaII_1 = $_POST['txtCalExtraBiologiaII_1'];
+    $txtFechaCalExtraBiologiaII_1 = $_POST['txtFechaCalExtraBiologiaII_1'];
+    $txtCalExtraBiologiaII_2 = $_POST['txtCalExtraBiologiaII_2'];
+    $txtFechaCalExtraBiologiaII_2 = $_POST['txtFechaCalExtraBiologiaII_2'];
+    $txtCalExtraBiologiaII_3 = $_POST['txtCalExtraBiologiaII_3'];
+    $txtFechaCalExtraBiologiaII_3 = $_POST['txtFechaCalExtraBiologiaII_3'];
+    $txtCalExtraBiologiaII_4 = $_POST['txtCalExtraBiologiaII_4'];
+    $txtFechaCalExtraBiologiaII_4 = $_POST['txtFechaCalExtraBiologiaII_4'];
+
+    $txtCalFisicaII_1 = $_POST['txtCalFisicaII_1'];
+    $txtCalFisicaII_2 = $_POST['txtCalFisicaII_2'];
+    $txtCalFisicaII_3 = $_POST['txtCalFisicaII_3'];
+    $txtFaltaFisicaII_1 = $_POST['txtFaltaFisicaII_1'];
+    $txtFaltaFisicaII_2 = $_POST['txtFaltaFisicaII_2'];
+    $txtFaltaFisicaII_3 = $_POST['txtFaltaFisicaII_3'];
+    $txtCalExtraFisicaII_1 = $_POST['txtCalExtraFisicaII_1'];
+    $txtFechaCalExtraFisicaII_1 = $_POST['txtFechaCalExtraFisicaII_1'];
+    $txtCalExtraFisicaII_2 = $_POST['txtCalExtraFisicaII_2'];
+    $txtFechaCalExtraFisicaII_2 = $_POST['txtFechaCalExtraFisicaII_2'];
+    $txtCalExtraFisicaII_3 = $_POST['txtCalExtraFisicaII_3'];
+    $txtFechaCalExtraFisicaII_3 = $_POST['txtFechaCalExtraFisicaII_3'];
+    $txtCalExtraFisicaII_4 = $_POST['txtCalExtraFisicaII_4'];
+    $txtFechaCalExtraFisicaII_4= $_POST['txtFechaCalExtraFisicaII_4'];
+
+    $txtCalHMII_1 = $_POST['txtCalHMII_1'];
+    $txtCalHMII_2 = $_POST['txtCalHMII_2'];
+    $txtCalHMII_3 = $_POST['txtCalHMII_3'];
+    $txtFaltaHMII_1= $_POST['txtFaltaHMII_1'];
+    $txtFaltaHMII_2= $_POST['txtFaltaHMII_2'];
+    $txtFaltaHMII_3= $_POST['txtFaltaHMII_3'];
+    $txtCalExtraHMII_1 = $_POST['txtCalExtraHMII_1'];
+    $txtFechaCalExtraHMII_1= $_POST['txtFechaCalExtraHMII_1'];
+    $txtCalExtraHMII_2= $_POST['txtCalExtraHMII_2'];
+    $txtFechaCalExtraHMII_2 = $_POST['txtFechaCalExtraHMII_2'];
+    $txtCalExtraHMII_3= $_POST['txtCalExtraHMII_3'];
+    $txtFechaCalExtraHMII_3 = $_POST['txtFechaCalExtraHMII_3'];
+    $txtCalExtraHMII_4= $_POST['txtCalExtraHMII_4'];
+    $txtFechaCalExtraHMII_4= $_POST['txtFechaCalExtraHMII_4'];
+
+    $txtCalInglesIV_1= $_POST['txtCalInglesIV_1'];
+    $txtCalInglesIV_2= $_POST['txtCalInglesIV_2'];
+    $txtCalInglesIV_3= $_POST['txtCalInglesIV_3'];
+    $txtFaltaInglesIV_1= $_POST['txtFaltaInglesIV_1'];
+    $txtFaltaInglesIV_2= $_POST['txtFaltaInglesIV_2'];
+    $txtFaltaInglesIV_3= $_POST['txtFaltaInglesIV_3'];
+    $txtCalExtraInglesIV_1 = $_POST['txtCalExtraInglesIV_1'];
+    $txtFechaCalExtraInglesIV_1 = $_POST['txtFechaCalExtraInglesIV_1'];
+    $txtCalExtraInglesIV_2= $_POST['txtCalExtraInglesIV_2'];
+    $txtFechaCalExtraInglesIV_2= $_POST['txtFechaCalExtraInglesIV_2'];
+    $txtCalExtraInglesIV_3= $_POST['txtCalExtraInglesIV_3'];
+    $txtFechaCalExtraInglesIV_3 = $_POST['txtFechaCalExtraInglesIV_3'];
+    $txtCalExtraInglesIV_4= $_POST['txtCalExtraInglesIV_4'];
+    $txtFechaCalExtraInglesIV_4= $_POST['txtFechaCalExtraInglesIV_4'];
+
+    $txtCalLiteraturaII_1 = $_POST['txtCalLiteraturaII_1'];
+    $txtCalLiteraturaII_2 = $_POST['txtCalLiteraturaII_2'];
+    $txtCalLiteraturaII_3 = $_POST['txtCalLiteraturaII_3'];
+    $txtFaltaLiteraturaII_1 = $_POST['txtFaltaLiteraturaII_1'];
+    $txtFaltaLiteraturaII_2 = $_POST['txtFaltaLiteraturaII_2'];
+    $txtFaltaLiteraturaII_3= $_POST['txtFaltaLiteraturaII_3'];
+    $txtCalExtraLiteraturaII_1= $_POST['txtCalExtraLiteraturaII_1'];
+    $txtFechaCalExtraLiteraturaII_1= $_POST['txtFechaCalExtraLiteraturaII_1'];
+    $txtCalExtraLiteraturaII_2= $_POST['txtCalExtraLiteraturaII_2'];
+    $txtFechaCalExtraLiteraturaII_2= $_POST['txtFechaCalExtraLiteraturaII_2'];
+    $txtCalExtraLiteraturaII_3= $_POST['txtCalExtraLiteraturaII_3'];
+    $txtFechaCalExtraLiteraturaII_3= $_POST['txtFechaCalExtraLiteraturaII_3'];
+    $txtCalExtraLiteraturaII_4= $_POST['txtCalExtraLiteraturaII_4'];
+    $txtFechaCalExtraLiteraturaII_4= $_POST['txtFechaCalExtraLiteraturaII_4'];
+
+    $txtCalCV_1 = $_POST['txtCalCV_1'];
+    $txtCalCV_2 = $_POST['txtCalCV_2'];
+    $txtCalCV_3 = $_POST['txtCalCV_3'];
+    $txtFaltaCV_1= $_POST['txtFaltaCV_1'];
+    $txtFaltaCV_2= $_POST['txtFaltaCV_2'];
+    $txtFaltaCV_3= $_POST['txtFaltaCV_3'];
+    $txtCalExtraCV_1= $_POST['txtCalExtraCV_1'];
+    $txtFechaCalExtraCV_1 = $_POST['txtFechaCalExtraCV_1'];
+    $txtCalExtraCV_2= $_POST['txtCalExtraCV_2'];
+    $txtFechaCalExtraCV_2 = $_POST['txtFechaCalExtraCV_2'];
+    $txtCalExtraCV_3 = $_POST['txtCalExtraCV_3'];
+    $txtFechaCalExtraCV_3 = $_POST['txtFechaCalExtraCV_3'];
+    $txtCalExtraCV_4= $_POST['txtCalExtraCV_4'];
+    $txtFechaCalExtraCV_4 = $_POST['txtFechaCalExtraCV_4'];
+
+    $txtCalMRC_1= $_POST['txtCalMRC_1'];
+    $txtCalMRC_2 = $_POST['txtCalMRC_2'];
+    $txtCalMRC_3 = $_POST['txtCalMRC_3'];
+    $txtFaltaMRC_1= $_POST['txtFaltaMRC_1'];
+    $txtFaltaMRC_2= $_POST['txtFaltaMRC_2'];
+    $txtFaltaMRC_3= $_POST['txtFaltaMRC_3'];
+    $txtCalExtraMRC_1= $_POST['txtCalExtraMRC_1'];
+    $txtFechaCalExtraMRC_1 = $_POST['txtFechaCalExtraMRC_1'];
+    $txtCalExtraMRC_2 = $_POST['txtCalExtraMRC_2'];
+    $txtFechaCalExtraMRC_2 = $_POST['txtFechaCalExtraMRC_2'];
+    $txtCalExtraMRC_3= $_POST['txtCalExtraMRC_3'];
+    $txtFechaCalExtraMRC_3= $_POST['txtFechaCalExtraMRC_3'];
+    $txtCalExtraMRC_4= $_POST['txtCalExtraMRC_4'];
+    $txtFechaCalExtraMRC_4= $_POST['txtFechaCalExtraMRC_4'];
+
+    $txtCalAFDRIV_1 = $_POST['txtCalAFDRIV_1'];
+    $txtCalAFDRIV_2 = $_POST['txtCalAFDRIV_2'];
+    $txtCalAFDRIV_3 = $_POST['txtCalAFDRIV_3'];
+    $txtFaltaAFDRIV_1= $_POST['txtFaltaAFDRIV_1'];
+    $txtFaltaAFDRIV_2= $_POST['txtFaltaAFDRIV_2'];
+    $txtFaltaAFDRIV_3= $_POST['txtFaltaAFDRIV_3'];
+    $txtCalExtraAFDRIV_1 = $_POST['txtCalExtraAFDRIV_1'];
+    $txtFechaCalExtraAFDRIV_1= $_POST['txtFechaCalExtraAFDRIV_1'];
+    $txtCalExtraAFDRIV_2= $_POST['txtCalExtraAFDRIV_2'];
+    $txtFechaCalExtraAFDRIV_2= $_POST['txtFechaCalExtraAFDRIV_2'];
+    $txtCalExtraAFDRIV_3= $_POST['txtCalExtraAFDRIV_3'];
+    $txtFechaCalExtraAFDRIV_3 = $_POST['txtFechaCalExtraAFDRIV_3'];
+    $txtCalExtraAFDRIV_4= $_POST['txtCalExtraAFDRIV_4'];
+    $txtFechaCalExtraAFDRIV_4= $_POST['txtFechaCalExtraAFDRIV_4'];
+
+    $txtCalSIAIV_1 = $_POST['txtCalSIAIV_1'];
+    $txtCalSIAIV_2 = $_POST['txtCalSIAIV_2'];
+    $txtCalSIAIV_3 = $_POST['txtCalSIAIV_3'];
+    $txtFaltaSIAIV_1= $_POST['txtFaltaSIAIV_1'];
+    $txtFaltaSIAIV_2= $_POST['txtFaltaSIAIV_2'];
+    $txtFaltaSIAIV_3= $_POST['txtFaltaSIAIV_3'];
+    $txtCalExtraSIAIV_1 = $_POST['txtCalExtraSIAIV_1'];
+    $txtFechaCalExtraSIAIV_1 = $_POST['txtFechaCalExtraSIAIV_1'];
+    $txtCalExtraSIAIV_2 = $_POST['txtCalExtraSIAIV_2'];
+    $txtFechaCalExtraSIAIV_2= $_POST['txtFechaCalExtraSIAIV_2'];
+    $txtCalExtraSIAIV_3= $_POST['txtCalExtraSIAIV_3'];
+    $txtFechaCalExtraSIAIV_3 = $_POST['txtFechaCalExtraSIAIV_3'];
+    $txtCalExtraSIAIV_4 = $_POST['txtCalExtraSIAIV_4'];
+    $txtFechaCalExtraSIAIV_4 = $_POST['txtFechaCalExtraSIAIV_4'];
+
+    $calificacionFinal1   = ($txtCalMateIV_3 + $txtCalMateIV_2 + $txtCalMateIV_1)/3;
+    $calificacionFinal2   = ($txtCalBiologiaII_3 + $txtCalBiologiaII_2 + $txtCalBiologiaII_1)/3;
+    $calificacionFinal3   = ($txtCalFisicaII_3 + $txtCalFisicaII_2 + $txtCalFisicaII_1)/3;
+    $calificacionFinal4   = ($txtCalHMII_3 + $txtCalHMII_2 + $txtCalHMII_1)/3;
+    $calificacionFinal5   = ($txtCalInglesIV_3 + $txtCalInglesIV_2 + $txtCalInglesIV_1)/3;
+    $calificacionFinal6   = ($txtCalLiteraturaII_3 + $txtCalLiteraturaII_2 + $txtCalLiteraturaII_1)/3;
+    $calificacionFinal7   = ($txtCalCV_3 + $txtCalCV_2 + $txtCalCV_1)/3;
+    $calificacionFinal8   = ($txtCalMRC_3 + $txtCalMRC_2 + $txtCalMRC_1)/3;
+    $calificacionFinal9   = ($txtCalAFDRIV_3 + $txtCalAFDRIV_2 + $txtCalAFDRIV_1)/3;
+    $calificacionFinal10  = ($txtCalSIAIV_3 + $txtCalSIAIV_2 + $txtCalSIAIV_1)/3;
+    $sentencia= "INSERT INTO `datosalumno-semestre4` VALUES 
+        (DEFAULT,1,'$matricula',$txtFaltaMateIV_1,$txtFaltaMateIV_2,$txtFaltaMateIV_3,$txtCalMateIV_1,$txtCalMateIV_2,$txtCalMateIV_3,$txtCalExtraMateIV_1,'$txtFechaCalExtraMateIV_1',$txtCalExtraMateIV_2,'$txtFechaCalExtraMateIV_2',$txtCalExtraMateIV_3,'$txtFechaCalExtraMateIV_3',$txtCalExtraMateIV_4,'$txtFechaCalExtraMateIV_4',$calificacionFinal1),
+        (DEFAULT,2,'$matricula',$txtFaltaBiologiaII_1,$txtFaltaBiologiaII_2,$txtFaltaBiologiaII_3,$txtCalBiologiaII_1,$txtCalBiologiaII_2,$txtCalBiologiaII_3,$txtCalExtraBiologiaII_1,'$txtFechaCalExtraBiologiaII_1',$txtCalExtraBiologiaII_2,'$txtFechaCalExtraBiologiaII_2',$txtCalExtraBiologiaII_3,'$txtFechaCalExtraBiologiaII_3',$txtCalExtraBiologiaII_4,'$txtFechaCalExtraBiologiaII_4',$calificacionFinal2),
+        (DEFAULT,3,'$matricula',$txtFaltaFisicaII_1,$txtFaltaFisicaII_2,$txtFaltaFisicaII_3,$txtCalFisicaII_1,$txtCalFisicaII_2,$txtCalFisicaII_3,$txtCalExtraFisicaII_1,'$txtFechaCalExtraFisicaII_1',$txtCalExtraFisicaII_2,'$txtFechaCalExtraFisicaII_2',$txtCalExtraFisicaII_3,'$txtFechaCalExtraFisicaII_3',$txtCalExtraFisicaII_4,'$txtFechaCalExtraFisicaII_4',$calificacionFinal3),
+        (DEFAULT,4,'$matricula',$txtFaltaHMII_1,$txtFaltaHMII_2,$txtFaltaHMII_3,$txtCalHMII_1,$txtCalHMII_2,$txtCalHMII_3,$txtCalExtraHMII_1,'$txtFechaCalExtraHMII_1',$txtCalExtraHMII_2,'$txtFechaCalExtraHMII_2',$txtCalExtraHMII_3,'$txtFechaCalExtraHMII_3',$txtCalExtraHMII_4,'$txtFechaCalExtraHMII_4',$calificacionFinal4),
+        (DEFAULT,5,'$matricula',$txtFaltaInglesIV_1,$txtFaltaInglesIV_2,$txtFaltaInglesIV_3,$txtCalInglesIV_1,$txtCalInglesIV_2,$txtCalInglesIV_3,$txtCalExtraInglesIV_1,'$txtFechaCalExtraInglesIV_1',$txtCalExtraInglesIV_2,'$txtFechaCalExtraInglesIV_2',$txtCalExtraInglesIV_3,'$txtFechaCalExtraInglesIV_3',$txtCalExtraInglesIV_4,'$txtFechaCalExtraInglesIV_4',$calificacionFinal5),
+        (DEFAULT,6,'$matricula',$txtFaltaLiteraturaII_1,$txtFaltaLiteraturaII_2,$txtFaltaLiteraturaII_3,$txtCalLiteraturaII_1,$txtCalLiteraturaII_2,$txtCalLiteraturaII_3,$txtCalExtraLiteraturaII_1,'$txtFechaCalExtraLiteraturaII_1',$txtCalExtraLiteraturaII_2,'$txtFechaCalExtraLiteraturaII_2',$txtCalExtraLiteraturaII_3,'$txtFechaCalExtraLiteraturaII_3',$txtCalExtraLiteraturaII_4,'$txtFechaCalExtraLiteraturaII_4',$calificacionFinal6),
+        (DEFAULT,7,'$matricula',$txtFaltaCV_1,$txtFaltaCV_2,$txtFaltaCV_3,$txtCalCV_1,$txtCalCV_2,$txtCalCV_3,$txtCalExtraCV_1,'$txtFechaCalExtraCV_1',$txtCalExtraCV_2,'$txtFechaCalExtraCV_2',$txtCalExtraCV_3,'$txtFechaCalExtraCV_3',$txtCalExtraCV_4,'$txtFechaCalExtraCV_4',$calificacionFinal7),
+        (DEFAULT,8,'$matricula',$txtFaltaMRC_1,$txtFaltaMRC_2,$txtFaltaMRC_3,$txtCalMRC_1,$txtCalMRC_2,$txtCalMRC_3,$txtCalExtraMRC_1,'$txtFechaCalExtraMRC_1',$txtCalExtraMRC_2,'$txtFechaCalExtraMRC_2',$txtCalExtraMRC_3,'$txtFechaCalExtraMRC_3',$txtCalExtraMRC_4,'$txtFechaCalExtraMRC_4',$calificacionFinal8),
+        (DEFAULT,9,'$matricula',$txtFaltaAFDRIV_1,$txtFaltaAFDRIV_2,$txtFaltaAFDRIV_3,$txtCalAFDRIV_1,$txtCalAFDRIV_2,$txtCalAFDRIV_3,$txtCalExtraAFDRIV_1,'$txtFechaCalExtraAFDRIV_1',$txtCalExtraAFDRIV_2,'$txtFechaCalExtraAFDRIV_2',$txtCalExtraAFDRIV_3,'$txtFechaCalExtraAFDRIV_3',$txtCalExtraAFDRIV_4,'$txtFechaCalExtraAFDRIV_4',$calificacionFinal9),
+        (DEFAULT,10,'$matricula',$txtFaltaSIAIV_1,$txtFaltaSIAIV_2,$txtFaltaSIAIV_3,$txtCalSIAIV_1,$txtCalSIAIV_2,$txtCalSIAIV_3,$txtCalExtraSIAIV_1,'$txtFechaCalExtraSIAIV_1',$txtCalExtraSIAIV_2,'$txtFechaCalExtraSIAIV_2',$txtCalExtraSIAIV_3,'$txtFechaCalExtraSIAIV_3',$txtCalExtraSIAIV_4,'$txtFechaCalExtraSIAIV_4',$calificacionFinal10);";
+    if(mysqli_query($conexion,$sentencia)){
+        header("Location: ../verCalificacionesAlumno.php");}
+    else{
+        echo "Error";
+    }
+    mysqli_close($conexion);        
+?>
