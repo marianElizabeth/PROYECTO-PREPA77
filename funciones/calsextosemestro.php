8e980c38773e0b78a964a281d5574340da3314b3
Vvictorvaldezz <victorhugovaldezrobles.01.11@gmail.com>
2021-07-29 23:45:25 -0500

    Update calsextosemestro.php

--- funciones/calsextosemestro.php
+++ funciones/calsextosemestro.php
@@ -62,20 +62,20 @@
     $txtCalExtraEconomiaII_4= $_POST['txtCalExtraEconomiaII_4'];
     $txtFechaCalExtraEconomiaII_4= $_POST['txtFechaCalExtraEconomiaII_4'];
 
-    $txtCalPDDcologiaII_1= $_POST['txtCalPDDcologiaII_1'];
-    $txtCalPDDcologiaII_2= $_POST['txtCalPDDcologiaII_2'];
-    $txtCalPDDcologiaII_3= $_POST['txtCalPDDcologiaII_3'];
-    $txtFaltaPDDcologiaII_1= $_POST['txtFaltaPDDcologiaII_1'];
-    $txtFaltaPDDcologiaII_2= $_POST['txtFaltaPDDcologiaII_2'];
-    $txtFaltaPDDcologiaII_3= $_POST['txtFaltaPDDcologiaII_3'];
-    $txtCalExtraPDDcologiaII_1 = $_POST['txtCalExtraPDDcologiaII_1'];
-    $txtFechaCalExtraPDDcologiaII_1 = $_POST['txtFechaCalExtraPDDcologiaII_1'];
-    $txtCalExtraPDDcologiaII_2= $_POST['txtCalExtraPDDcologiaII_2'];
-    $txtFechaCalExtraPDDcologiaII_2= $_POST['txtFechaCalExtraPDDcologiaII_2'];
-    $txtCalExtraPDDcologiaII_4= $_POST['txtCalExtraPDDcologiaII_3'];
-    $txtFechaCalExtraPDDcologiaII_3 = $_POST['txtFechaCalExtraPDDcologiaII_3'];
-    $txtCalExtraPDDcologiaII_4= $_POST['txtCalExtraPDDcologiaII_4'];
-    $txtFechaCalExtraPDDcologiaII_4= $_POST['txtFechaCalExtraPDDcologiaII_4'];
+    $txtCalPsicologiaII_1= $_POST['txtCalPsicologiaII_1'];
+    $txtCalPsicologiaII_2= $_POST['txtCalPsicologiaII_2'];
+    $txtCalPsicologiaII_3= $_POST['txtCalPsicologiaII_3'];
+    $txtFaltaPsicologiaII_1= $_POST['txtFaltaPsicologiaII_1'];
+    $txtFaltaPsicologiaII_2= $_POST['txtFaltaPsicologiaII_2'];
+    $txtFaltaPsicologiaII_3= $_POST['txtFaltaPsicologiaII_3'];
+    $txtCalExtraPsicologiaII_1 = $_POST['txtCalExtraPsicologiaII_1'];
+    $txtFechaCalExtraPsicologiaII_1 = $_POST['txtFechaCalExtraPsicologiaII_1'];
+    $txtCalExtraPsicologiaII_2= $_POST['txtCalExtraPsicologiaII_2'];
+    $txtFechaCalExtraPsicologiaII_2= $_POST['txtFechaCalExtraPsicologiaII_2'];
+    $txtCalExtraPsicologiaII_4= $_POST['txtCalExtraPsicologiaII_3'];
+    $txtFechaCalExtraPsicologiaII_3 = $_POST['txtFechaCalExtraPsicologiaII_3'];
+    $txtCalExtraPsicologiaII_4= $_POST['txtCalExtraPsicologiaII_4'];
+    $txtFechaCalExtraPsicologiaII_4= $_POST['txtFechaCalExtraPsicologiaII_4'];
 
     $txtCalDerechoII_1 = $_POST['txtCalDerechoII_1'];
     $txtCalDerechoII_2 = $_POST['txtCalDerechoII_2'];
@@ -121,6 +121,21 @@
     $txtFechaCalExtraFilosofia_3= $_POST['txtFechaCalExtraFilosofia_3'];
     $txtCalExtraFilosofia_4= $_POST['txtCalExtraFilosofia_4'];
     $txtFechaCalExtraFilosofia_4= $_POST['txtFechaCalExtraFilosofia_4'];
+
+    $txtCalPW_1 = $_POST['txtCalPW_1'];
+    $txtCalPW_2 = $_POST['txtCalPW_2'];
+    $txtCalPW_3 = $_POST['txtCalPW_3'];
+    $txtFaltaPW_1= $_POST['txtFaltaPW_1'];
+    $txtFaltaPW_2= $_POST['txtFaltaPW_2'];
+    $txtFaltaPW_3= $_POST['txtFaltaPW_3'];
+    $txtCalExtraPW_1 = $_POST['txtCalExtraPW_1'];
+    $txtFechaCalExtraPW_1= $_POST['txtFechaCalExtraPW_1'];
+    $txtCalExtraPW_2= $_POST['txtCalExtraPW_2'];
+    $txtFechaCalExtraPW_2= $_POST['txtFechaCalExtraPW_2'];
+    $txtCalExtraPW_3= $_POST['txtCalExtraPW_3'];
+    $txtFechaCalExtraPW_3 = $_POST['txtFechaCalExtraPW_3'];
+    $txtCalExtraPW_4= $_POST['txtCalExtraPW_4'];
+    $txtFechaCalExtraPW_4= $_POST['txtFechaCalExtraPW_4'];
 
     $txtCalDD_1 = $_POST['txtCalDD_1'];
     $txtCalDD_2 = $_POST['txtCalDD_2'];
@@ -171,25 +186,28 @@
     $calFilosofiaicacionFinal2   = ($txtCalEMA_3 + $txtCalEMA_2 + $txtCalEMA_1)/3;
     $calFilosofiaicacionFinal3   = ($txtCalHUC_3 + $txtCalHUC_2 + $txtCalHUC_1)/3;
     $calFilosofiaicacionFinal4   = ($txtCalEconomiaII_3 + $txtCalEconomiaII_2 + $txtCalEconomiaII_1)/3;
-    $calFilosofiaicacionFinal5   = ($txtCalPDDcologiaII_3 + $txtCalPDDcologiaII_2 + $txtCalPDDcologiaII_1)/3;
+    $calFilosofiaicacionFinal5   = ($txtCalPsicologiaII_3 + $txtCalPsicologiaII_2 + $txtCalPsicologiaII_1)/3;
     $calFilosofiaicacionFinal6   = ($txtCalDerechoII_3 + $txtCalDerechoII_2 + $txtCalDerechoII_1)/3;
     $calFilosofiaicacionFinal7   = ($txtCalInglesVI_3 + $txtCalInglesVI_2 + $txtCalInglesVI_1)/3;
     $calFilosofiaicacionFinal8   = ($txtCalFilosofia_3 + $txtCalFilosofia_2 + $txtCalFilosofia_1)/3;
-    $calFilosofiaicacionFinal9   = ($txtCalDD_3 + $txtCalDD_2 + $txtCalDD_1)/3;
-    $calFilosofiaicacionFinal10  = ($txtCalAACIV_3 + $txtCalAACIV_2 + $txtCalAACIV_1)/3;
-    $calFilosofiaicacionFinal11  = ($txtCalSIAVI_3 + $txtCalSIAVI_2 + $txtCalSIAVI_1)/3;
+    $calFilosofiaicacionFinal9  = ($txtCalPW_3 + $txtCalPW_2 + $txtCalPW_1)/3;
+    $calFilosofiaicacionFinal10   = ($txtCalDD_3 + $txtCalDD_2 + $txtCalDD_1)/3;
+    $calFilosofiaicacionFinal11  = ($txtCalAACIV_3 + $txtCalAACIV_2 + $txtCalAACIV_1)/3;
+    $calFilosofiaicacionFinal12  = ($txtCalSIAVI_3 + $txtCalSIAVI_2 + $txtCalSIAVI_1)/3;
+    
     $sentencia= "INSERT INTO `datosalumno-semestre6` VALUES 
         (DEFAULT,1,'$matricula',$txtFaltaMateVI_1,$txtFaltaMateVI_2,$txtFaltaMateVI_3,$txtCalMateVI_1,$txtCalMateVI_2,$txtCalMateVI_3,$txtCalExtraMateVI_1,'$txtFechaCalExtraMateVI_1',$txtCalExtraMateVI_2,'$txtFechaCalExtraMateVI_2',$txtCalExtraMateVI_3,'$txtFechaCalExtraMateVI_3',$txtCalExtraMateVI_4,'$txtFechaCalExtraMateVI_4',$calFilosofiaicacionFinal1),
         (DEFAULT,2,'$matricula',$txtFaltaEMA_1,$txtFaltaEMA_2,$txtFaltaEMA_3,$txtCalEMA_1,$txtCalEMA_2,$txtCalEMA_3,$txtCalExtraEMA_1,'$txtFechaCalExtraEMA_1',$txtCalExtraEMA_2,'$txtFechaCalExtraEMA_2',$txtCalExtraEMA_3,'$txtFechaCalExtraEMA_3',$txtCalExtraEMA_4,'$txtFechaCalExtraEMA_4',$calFilosofiaicacionFinal2),
         (DEFAULT,3,'$matricula',$txtFaltaHUC_1,$txtFaltaHUC_2,$txtFaltaHUC_3,$txtCalHUC_1,$txtCalHUC_2,$txtCalHUC_3,$txtCalExtraHUC_1,'$txtFechaCalExtraHUC_1',$txtCalExtraHUC_2,'$txtFechaCalExtraHUC_2',$txtCalExtraHUC_3,'$txtFechaCalExtraHUC_3',$txtCalExtraHUC_4,'$txtFechaCalExtraHUC_4',$calFilosofiaicacionFinal3),
         (DEFAULT,4,'$matricula',$txtFaltaEconomiaII_1,$txtFaltaEconomiaII_2,$txtFaltaEconomiaII_3,$txtCalEconomiaII_1,$txtCalEconomiaII_2,$txtCalEconomiaII_3,$txtCalExtraEconomiaII_1,'$txtFechaCalExtraEconomiaII_1',$txtCalExtraEconomiaII_2,'$txtFechaCalExtraEconomiaII_2',$txtCalExtraEconomiaII_3,'$txtFechaCalExtraEconomiaII_3',$txtCalExtraEconomiaII_4,'$txtFechaCalExtraEconomiaII_4',$calFilosofiaicacionFinal4),
-        (DEFAULT,5,'$matricula',$txtFaltaPDDcologiaII_1,$txtFaltaPDDcologiaII_2,$txtFaltaPDDcologiaII_3,$txtCalPDDcologiaII_1,$txtCalPDDcologiaII_2,$txtCalPDDcologiaII_3,$txtCalExtraPDDcologiaII_1,'$txtFechaCalExtraPDDcologiaII_1',$txtCalExtraPDDcologiaII_2,'$txtFechaCalExtraPDDcologiaII_2',$txtCalExtraPDDcologiaII_3,'$txtFechaCalExtraPDDcologiaII_3',$txtCalExtraPDDcologiaII_4,'$txtFechaCalExtraPDDcologiaII_4',$calFilosofiaicacionFinal5),
+        (DEFAULT,5,'$matricula',$txtFaltaPsicologiaII_1,$txtFaltaPsicologiaII_2,$txtFaltaPsicologiaII_3,$txtCalPsicologiaII_1,$txtCalPsicologiaII_2,$txtCalPsicologiaII_3,$txtCalExtraPsicologiaII_1,'$txtFechaCalExtraPsicologiaII_1',$txtCalExtraPsicologiaII_2,'$txtFechaCalExtraPsicologiaII_2',$txtCalExtraPsicologiaII_3,'$txtFechaCalExtraPsicologiaII_3',$txtCalExtraPsicologiaII_4,'$txtFechaCalExtraPsicologiaII_4',$calFilosofiaicacionFinal5),
         (DEFAULT,6,'$matricula',$txtFaltaDerechoII_1,$txtFaltaDerechoII_2,$txtFaltaDerechoII_3,$txtCalDerechoII_1,$txtCalDerechoII_2,$txtCalDerechoII_3,$txtCalExtraDerechoII_1,'$txtFechaCalExtraDerechoII_1',$txtCalExtraDerechoII_2,'$txtFechaCalExtraDerechoII_2',$txtCalExtraDerechoII_3,'$txtFechaCalExtraDerechoII_3',$txtCalExtraDerechoII_4,'$txtFechaCalExtraDerechoII_4',$calFilosofiaicacionFinal6),
         (DEFAULT,7,'$matricula',$txtFaltaInglesVI_1,$txtFaltaInglesVI_2,$txtFaltaInglesVI_3,$txtCalInglesVI_1,$txtCalInglesVI_2,$txtCalInglesVI_3,$txtCalExtraInglesVI_1,'$txtFechaCalExtraInglesVI_1',$txtCalExtraInglesVI_2,'$txtFechaCalExtraInglesVI_2',$txtCalExtraInglesVI_3,'$txtFechaCalExtraInglesVI_3',$txtCalExtraInglesVI_4,'$txtFechaCalExtraInglesVI_4',$calFilosofiaicacionFinal7),
         (DEFAULT,8,'$matricula',$txtFaltaFilosofia_1,$txtFaltaFilosofia_2,$txtFaltaFilosofia_3,$txtCalFilosofia_1,$txtCalFilosofia_2,$txtCalFilosofia_3,$txtCalExtraFilosofia_1,'$txtFechaCalExtraFilosofia_1',$txtCalExtraFilosofia_2,'$txtFechaCalExtraFilosofia_2',$txtCalExtraFilosofia_3,'$txtFechaCalExtraFilosofia_3',$txtCalExtraFilosofia_4,'$txtFechaCalExtraFilosofia_4',$calFilosofiaicacionFinal8),
-        (DEFAULT,9,'$matricula',$txtFaltaDD_1,$txtFaltaDD_2,$txtFaltaDD_3,$txtCalDD_1,$txtCalDD_2,$txtCalDD_3,$txtCalExtraDD_1,'$txtFechaCalExtraDD_1',$txtCalExtraDD_2,'$txtFechaCalExtraDD_2',$txtCalExtraDD_3,'$txtFechaCalExtraDD_3',$txtCalExtraDD_4,'$txtFechaCalExtraDD_4',$calFilosofiaicacionFinal9),
-        (DEFAULT,10,'$matricula',$txtFaltaAACIV_1,$txtFaltaAACIV_2,$txtFaltaAACIV_3,$txtCalAACIV_1,$txtCalAACIV_2,$txtCalAACIV_3,$txtCalExtraAACIV_1,'$txtFechaCalExtraAACIV_1',$txtCalExtraAACIV_2,'$txtFechaCalExtraAACIV_2',$txtCalExtraAACIV_3,'$txtFechaCalExtraAACIV_3',$txtCalExtraAACIV_4,'$txtFechaCalExtraAACIV_4',$calFilosofiaicacionFinal10),
-        (DEFAULT,11,'$matricula',$txtFaltaSIAVI_1,$txtFaltaSIAVI_2,$txtFaltaSIAVI_3,$txtCalSIAVI_1,$txtCalSIAVI_2,$txtCalSIAVI_3,$txtCalExtraSIAVI_1,'$txtFechaCalExtraSIAVI_1',$txtCalExtraSIAVI_2,'$txtFechaCalExtraSIAVI_2',$txtCalExtraSIAVI_3,'$txtFechaCalExtraSIAVI_3',$txtCalExtraSIAVI_4,'$txtFechaCalExtraSIAVI_4',$calFilosofiaicacionFinal11)        
+        (DEFAULT,9,'$matricula',$txtFaltaPW_1,$txtFaltaPW_2,$txtFaltaPW_3,$txtCalPW_1,$txtCalPW_2,$txtCalPW_3,$txtCalExtraPW_1,'$txtFechaCalExtraPW_1',$txtCalExtraPW_2,'$txtFechaCalExtraPW_2',$txtCalExtraPW_3,'$txtFechaCalExtraPW_3',$txtCalExtraPW_4,'$txtFechaCalExtraPW_4',$calFilosofiaicacionFinal9),
+        (DEFAULT,10,'$matricula',$txtFaltaDD_1,$txtFaltaDD_2,$txtFaltaDD_3,$txtCalDD_1,$txtCalDD_2,$txtCalDD_3,$txtCalExtraDD_1,'$txtFechaCalExtraDD_1',$txtCalExtraDD_2,'$txtFechaCalExtraDD_2',$txtCalExtraDD_3,'$txtFechaCalExtraDD_3',$txtCalExtraDD_4,'$txtFechaCalExtraDD_4',$calFilosofiaicacionFinal10),
+        (DEFAULT,11,'$matricula',$txtFaltaAACIV_1,$txtFaltaAACIV_2,$txtFaltaAACIV_3,$txtCalAACIV_1,$txtCalAACIV_2,$txtCalAACIV_3,$txtCalExtraAACIV_1,'$txtFechaCalExtraAACIV_1',$txtCalExtraAACIV_2,'$txtFechaCalExtraAACIV_2',$txtCalExtraAACIV_3,'$txtFechaCalExtraAACIV_3',$txtCalExtraAACIV_4,'$txtFechaCalExtraAACIV_4',$calFilosofiaicacionFinal11),        
+        (DEFAULT,12,'$matricula',$txtFaltaSIAVI_1,$txtFaltaSIAVI_2,$txtFaltaSIAVI_3,$txtCalSIAVI_1,$txtCalSIAVI_2,$txtCalSIAVI_3,$txtCalExtraSIAVI_1,'$txtFechaCalExtraSIAVI_1',$txtCalExtraSIAVI_2,'$txtFechaCalExtraSIAVI_2',$txtCalExtraSIAVI_3,'$txtFechaCalExtraSIAVI_3',$txtCalExtraSIAVI_4,'$txtFechaCalExtraSIAVI_4',$calFilosofiaicacionFinal12)
         ;";
     if(mysqli_query($conexion,$sentencia)){
         header("Location: ../verCalificacionesAlumno.php");}
